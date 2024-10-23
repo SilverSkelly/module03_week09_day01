06bd7be263c43146ba3ba8f6a25d95a7d60dc890
SilverSkelly <129800054+SilverSkelly@users.noreply.github.com>
2024-10-23 16:17:20 -0500

    u

---
+++ main.js
@@ -0,0 +1,13 @@
+// Operations: Object Access, Invocation
+console.log("What's your name?");
+
+// Operations: object access, object access, invocation, invocation,
+process.stdin.on('data', function(data) {
+
+    // Operations: assignment, object access, object access
+    const name = data.toString().trim();
+    
+    // Operations: object access, invocation, invocation,
+    console.log(`Hello, ${name}!`);
+
+});
